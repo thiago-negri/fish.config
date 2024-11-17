29d604719a342de75c5b3bac028e267a4604819b
Thiago Negri <evohunz@gmail.com>
2024-11-17 07:28:33 -0300

    First commit

---
+++ config.fish
@@ -0,0 +1,23 @@
+if status is-interactive
+  # Commands to run in interactive sessions can go here
+end
+
+function wezsetcwd --on-event fish_prompt
+  wezterm set-working-directory
+end
+
+function fish_rm_path --argument path
+    set path (path resolve $path)
+    set path_index (contains -i $path $fish_user_paths)
+
+    if test $status -ne 0
+        echo $path not in fish_user_paths
+        return 1
+    end
+
+    echo Removing $path at index $path_index from fish_user_paths
+
+    set -e fish_user_paths[$path_index]
+end
+
+alias l "ls -la"
